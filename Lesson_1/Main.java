fca43c1dd88e9bd307d54f612ce38b272671ca3f
Mintemir <smint18@yandex.ru>
2024-07-14 18:25:05 +0300

    implement HW_1

---
+++ Lesson_1/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    
+    public static void main(String[] args) {
+    // TODO: просуммировать числа в диапазоне от 1 - 5 
+        
+        int sum = 1 + 2;
+        int sum1 = sum + 3;
+        int sum2 = sum1 + 4;
+        int result = sum2 +5;
+        
+        System.out.println(result);
+
+        
+
+    }
+}
