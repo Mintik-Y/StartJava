71cdb88317e13563d3a197785ccde98eed31392a
Mintemir <smint18@yandex.ru>
2024-10-31 16:32:44 +0300

    implement HW_1

---
+++ Lesson_1/VariableNamesTheme.java
@@ -0,0 +1,186 @@
+public class VariableNamesTheme {
+    public static void main(String[] args) {
+        // Task 1
+        System.out.println("Задача 1. Разные переменные");
+        // Цифра
+        int num = 1;
+        System.out.println("Цифра = " + num);
+
+        // Сумма чисел
+        int sumDigits = 6;
+        System.out.println("Сумма чисел = " + sumDigits);
+
+        // Произведение цифр
+        int productNumbers = 7 * 7;
+        System.out.println("Произведение цифр = " + productNumbers);
+
+        // Максимальное число
+        int maxNum = 2147483647;
+        System.out.println("Максимальное число = " + maxNum);
+
+        // Количество десятков
+        int numberOfTens = 150 / 10;
+        System.out.println("Количество десятков = " + numberOfTens);
+
+        // Вес собаки
+        float weightDog = 43.58f;
+        System.out.println("Вес собаки = " + weightDog);
+
+        // Исходное число
+        int originalNumber = 32;
+        System.out.println("Исходное число = " + originalNumber);
+
+        // Процент по вкладу
+        float precentDeposit = 22.5f;
+        System.out.println("Процент по вкладу = " + precentDeposit);
+
+        // Переменная хранит символ &;
+        char symbol = '&';
+        System.out.println("Переменная хранит символ = " + symbol);
+
+        // Код ошибки
+        int errorCode = 404;
+        System.out.println("Код ошибки = " + errorCode);
+
+        // Тип сообщения
+        int typeMessage = 2;
+        System.out.println("Тип сообщения = " + typeMessage);
+
+        // Число нулей
+        int sumZero = 4;
+        System.out.println("Число нулей = " + sumZero);
+
+        // Уникальное число
+        int specialNum = 18;
+        System.out.println("Уникальное число = " + specialNum);
+
+        // Случайное число
+        int randomNum = 195012;
+        System.out.println("Случайное число = " + randomNum);
+
+        // Математическое выражение 
+        int mathExpression = 49 / 7;
+        System.out.println("Математическое выражение = " + mathExpression);
+
+        // Счет в игре
+        int scoreGame = 5;
+        System.out.println("Счет в игре = " + scoreGame);
+
+        // Максимальная длина
+        float maxLenght = 3.42f;
+        System.out.println("Максимальная длина = " + maxLenght);
+
+        // Пункт меню
+        int menuItem = 3;
+        System.out.println("Пункт меню = " + menuItem);
+
+        // Стоимость кофе
+        int costOfCoffee = 300;
+        System.out.println("Стоимость кофе = " + costOfCoffee);
+
+        // Дата начала
+        int startDate = 23;
+        System.out.println("Дата начала = " + startDate);
+
+        // Окончание диапазона
+        int endOfRange = 99;
+        System.out.println("Окончание диапазона = " + endOfRange);
+
+        // Полное имя работника месяца
+        int fullNameWorkerMonth = 15;
+        System.out.println("Полное имя работника месяца = " + fullNameWorkerMonth);
+
+        // Заголовок электронной книги
+        int bookTitle = 1;
+        System.out.println("Заголовок электронной книги = " + bookTitle);
+
+        // Размер
+        int size = 46;
+        System.out.println("Размер = " + size);
+
+        // Вместимость
+        int capacity = 120;
+        System.out.println("Вместимость ресторана = " + capacity + " человек ");
+
+        // Счетчик
+        int counter = 32;
+        System.out.println("Счетчик = " + counter);
+
+        // Путь до файла
+        int pathToFile = 5;
+        System.out.println("Путь до файла = " + pathToFile);
+
+        // Количество чисел в каждой строке
+        int numEachLine = 3212;
+        System.out.println("Количество чисел в каждой строке = " + numEachLine + "\n");
+
+        // Task 2
+        System.out.println("Задача 2. Boolean-переменные");
+        // Сотни равны?
+        boolean hasEqualHundreds = true;
+        System.out.println("Сотни равны ? - " + hasEqualHundreds );
+
+        // Компьютер включен?
+        boolean isComputerOn = false;
+        System.out.println("Компьютер включен ? - " + isComputerOn);
+
+        // Есть равные цифры?
+        boolean hasEqualDigits = true;
+        System.out.println("Есть равные цифры ? - " + hasEqualDigits);
+
+        // Создано?
+        boolean isCreate = false;
+        System.out.println("Создано ? - " + isCreate);
+
+        // Пустое?
+        boolean isEmpty = true;
+        System.out.println("Пустое ? - " + isEmpty);
+
+        // Активное?
+        boolean isActive = true;
+        System.out.println("Активное ? - " + isActive);
+
+        // Новое?
+        boolean isNew = false;
+        System.out.println("Новое ? - " + isNew);
+
+        // Электронная почта действительна? 
+        boolean isValidEmail = true;
+        System.out.println("Электронная почта действительна ? - " + isValidEmail);
+
+        // Имеются уникальные строки?
+        boolean isUniqueString = true;
+        System.out.println("Имеются уникальные строки ? - " + isUniqueString + "\n");
+
+        //Task 3
+        System.out.println("Задача 3. Аббревиатуры");
+        // Старый universally unique identifier
+        int oldUuid = 912488;
+        System.out.println("Старый universally unique identifier = " + oldUuid);
+
+        // производитель оперативной памяти
+        String prodRam = "Kingston";
+        System.out.println("Производитель оперативной памяти : " + prodRam);
+
+        // Емкость жесткого диска
+        int hddCapacity = 256;
+        System.out.println("Емкость жесткого диска = " + hddCapacity + " гб. ");
+
+        // Протокол передачи гипертекста
+        int http = 4;
+        System.out.println("Протокол передачи гипертекста = " + http);
+
+        // сокращенный uniform resource locator
+        int url = 53;
+        System.out.println("Cокращенный uniform resource locator = " + url);
+
+        // Новый идентификатор клиента
+        int newId = 1023;
+        System.out.println("Новый идентификатор клиента = " + newId);
+
+        // american standard code for information interchange
+        int ascii = 123;
+        System.out.println("ASCII = " + ascii + "\n");
+
+    }
+}
